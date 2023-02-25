fb438a87556c4f989af36a467f3c70dc9cfa021d
Bijan1986 <nayak.bijan@gmail.com>
2023-02-25 22:11:00 +0200

    initial commit

---
+++ functional-programming/src/main/java/com/practice/chapter1/DuplicateCharactersDemo.java
@@ -0,0 +1,15 @@
+package com.practice.chapter1;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DuplicateCharactersDemo {
+    public static void main(String[] args) {
+        System.out.println(findDuplicateChars("Bijan Nayak"));
+
+    }
+
+    public static Map<Character, Long> findDuplicateChars(String value) {
+        return value.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(character -> character, Collectors.counting()));
+    }
+}
